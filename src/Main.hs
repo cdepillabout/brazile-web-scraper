b73481d7c4fa9b3003144d525ace58bc75848be4
(cdep)illabout <cdep.illabout@gmail.com>
2014-09-30 07:51:01 +0900

    Updated to handle errors correctly.

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,16 @@
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 
-import Network.HTTP.Client (BodyReader, defaultManagerSettings, Manager, managerConnCount, managerResponseTimeout, parseUrl, Request, Response, responseBody, withManager, withResponse)
+import Control.Exception (catch, handle, IOException)
+import Network.HTTP.Client (BodyReader, defaultManagerSettings, HttpException, Manager, managerConnCount, managerResponseTimeout, parseUrl, Request, Response, responseBody, withManager, withResponse)
 import qualified Data.ByteString.Lazy as ByteStringLazy
 import qualified Data.ByteString as ByteStringStrict
 import System.IO (withFile, IOMode(WriteMode))
 import Text.Printf (printf)
 
+
+-------------------------------------------------------------------
+-- Constants
+-------------------------------------------------------------------
 
 siteUrl :: String
 siteUrl = "http://museudaimigracao.org.br/acervodigital/livros.php?pesq=1&nome=&sobrenome=&chegada=&vapor=&nacionalidade=&pagina="
@@ -27,9 +33,18 @@ concurrentConnectionCount = 40
 responseTimeoutSeconds :: Int
 responseTimeoutSeconds = 60
 
+-------------------------------------------------------------------
+-- Helper functions
+-------------------------------------------------------------------
+
 outFilePath :: Int -> FilePath
 outFilePath num = printf "output/site/%06d.html" num
 
+errFilePath :: Int -> FilePath
+errFilePath num = printf "output/err/%06d.html" num
+
+request :: Int -> IO Request
+request pageNum = parseUrl $ siteUrl ++ show pageNum
 
 brReadSome :: BodyReader -> Int -> IO ByteStringLazy.ByteString
 brReadSome brRead =
@@ -43,6 +58,30 @@ brReadSome brRead =
                 then return $ ByteStringLazy.fromChunks $ front []
                 else loop (front . (bs:)) (remainder - ByteStringStrict.length bs)
 
+-------------------------------------------------------------------
+-- file downloading functions
+-------------------------------------------------------------------
+
+doRequests :: Manager -> IO ()
+doRequests manager = do
+    let reqNum = 1
+    req <- request reqNum
+    catch (withResponse req manager $ doResponse reqNum)
+          (\err -> writeFile (errFilePath reqNum) $ show (err::HttpException) ++ "\n")
+
+doResponse :: Int -> Response BodyReader -> IO ()
+doResponse reqNum response = do
+    handle (\err -> writeFile (errFilePath reqNum) $ show (err::IOException) ++ "\n")
+           (withFile (outFilePath reqNum) WriteMode $ \fileHandle -> do
+              let lazybody = responseBody response
+              body <- brReadSome lazybody downloadLimitBytes
+              ByteStringLazy.hPut fileHandle body
+              ByteStringLazy.hPut fileHandle "\n")
+
+-------------------------------------------------------------------
+-- main
+-------------------------------------------------------------------
+
 main :: IO ()
 main = do
     let managerSettings = defaultManagerSettings
@@ -50,20 +89,4 @@ main = do
           , managerResponseTimeout = Just $ responseTimeoutSeconds * 1000000
           }
     withManager managerSettings doRequests
-  where
-    request :: Int -> IO Request
-    request pageNum = parseUrl $ siteUrl ++ show pageNum
-
-    doRequests :: Manager -> IO ()
-    doRequests manager = do
-        req <- request 1
-        withResponse req manager doResponse
-
-    doResponse :: Response BodyReader -> IO ()
-    doResponse response = do
-        withFile (outFilePath 1) WriteMode $ \handle -> do
-          let lazybody = responseBody response
-          body <- brReadSome lazybody downloadLimitBytes
-          ByteStringLazy.hPut handle body
-          putStrLn ""
 
