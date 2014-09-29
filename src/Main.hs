2f269abca896fe43896c18fd7be79db6b608ec9d
(cdep)illabout <cdep.illabout@gmail.com>
2014-09-30 06:35:32 +0900

    Make work for one page.

--- src/Main.hs
+++ src/Main.hs
@@ -2,10 +2,33 @@
 import Network.HTTP.Client (BodyReader, defaultManagerSettings, Manager, managerConnCount, managerResponseTimeout, parseUrl, Request, Response, responseBody, withManager, withResponse)
 import qualified Data.ByteString.Lazy as ByteStringLazy
 import qualified Data.ByteString as ByteStringStrict
+import System.IO (withFile, IOMode(WriteMode))
+import Text.Printf (printf)
 
 
 siteUrl :: String
 siteUrl = "http://museudaimigracao.org.br/acervodigital/livros.php?pesq=1&nome=&sobrenome=&chegada=&vapor=&nacionalidade=&pagina="
+
+start :: Int
+start = 1
+
+total :: Int
+total = 132028
+
+printStatusEvery :: Int
+printStatusEvery = 10
+
+downloadLimitBytes :: Int
+downloadLimitBytes = 50000
+
+concurrentConnectionCount :: Int
+concurrentConnectionCount = 40
+
+responseTimeoutSeconds :: Int
+responseTimeoutSeconds = 60
+
+outFilePath :: Int -> FilePath
+outFilePath num = printf "output/site/%06d.html" num
 
 
 brReadSome :: BodyReader -> Int -> IO ByteStringLazy.ByteString
@@ -22,23 +45,25 @@ brReadSome brRead =
 
 main :: IO ()
 main = do
-    let managerSettings = defaultManagerSettings { managerConnCount = 10
-                                                 , managerResponseTimeout = Just 60000000
-                                                 }
+    let managerSettings = defaultManagerSettings
+          { managerConnCount = concurrentConnectionCount
+          , managerResponseTimeout = Just $ responseTimeoutSeconds * 1000000
+          }
     withManager managerSettings doRequests
   where
-    request :: IO Request
-    request = parseUrl $ siteUrl ++ show (1::Integer)
+    request :: Int -> IO Request
+    request pageNum = parseUrl $ siteUrl ++ show pageNum
 
     doRequests :: Manager -> IO ()
     doRequests manager = do
-        req <- request
+        req <- request 1
         withResponse req manager doResponse
 
     doResponse :: Response BodyReader -> IO ()
     doResponse response = do
-        let lazybody = responseBody response
-        body <- brReadSome lazybody 100
-        ByteStringLazy.putStr body
-        putStrLn ""
+        withFile (outFilePath 1) WriteMode $ \handle -> do
+          let lazybody = responseBody response
+          body <- brReadSome lazybody downloadLimitBytes
+          ByteStringLazy.hPut handle body
+          putStrLn ""
 
