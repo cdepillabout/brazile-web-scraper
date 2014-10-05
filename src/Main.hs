698d51e551639ac29f4eb00d277b566b0d5e4a1d
(cdep)illabout <cdep.illabout@gmail.com>
2014-10-05 23:10:51 +0900

    Fetching all failed pages.

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 
-import Control.Concurrent (forkIO)
+import Control.Concurrent (forkIO, threadDelay)
 import Control.Concurrent.Chan (Chan, newChan, readChan, writeChan, writeList2Chan)
 import Control.Exception (catch, IOException)
 import Control.Monad (forever, forM_, when)
@@ -26,7 +26,98 @@ total :: Int
 total = 132028
 
 allRequestNumbers :: [Int]
-allRequestNumbers = [start .. total]
+--allRequestNumbers = [start .. total]
+allRequestNumbers = [ 001708
+                    , 001709
+                    , 001710
+                    , 001711
+                    , 026100
+                    , 026101
+                    , 026102
+                    , 026103
+                    , 032079
+                    , 032080
+                    , 032081
+                    , 044379
+                    , 044380
+                    , 044381
+                    , 047975
+                    , 047976
+                    , 047977
+                    , 050686
+                    , 050687
+                    , 050688
+                    , 050689
+                    , 050690
+                    , 059745
+                    , 061557
+                    , 071152
+                    , 071153
+                    , 071154
+                    , 086170
+                    , 086172
+                    , 086177
+                    , 086178
+                    , 086181
+                    , 086182
+                    , 086183
+                    , 086184
+                    , 086185
+                    , 086186
+                    , 086188
+                    , 086190
+                    , 086191
+                    , 086193
+                    , 086194
+                    , 086195
+                    , 086196
+                    , 086197
+                    , 086198
+                    , 086199
+                    , 086200
+                    , 086202
+                    , 086203
+                    , 086205
+                    , 086216
+                    , 086217
+                    , 086219
+                    , 086224
+                    , 086226
+                    , 086227
+                    , 086229
+                    , 086232
+                    , 086236
+                    , 086237
+                    , 086238
+                    , 086239
+                    , 086240
+                    , 086241
+                    , 086242
+                    , 086243
+                    , 086247
+                    , 095215
+                    , 100764
+                    , 105081
+                    , 105082
+                    , 105083
+                    , 105084
+                    , 105273
+                    , 106132
+                    , 106133
+                    , 107832
+                    , 107833
+                    , 107834
+                    , 107835
+                    , 107836
+                    , 107837
+                    , 107838
+                    , 107842
+                    , 107843
+                    , 109297
+                    , 118011
+                    , 118012
+                    , 118013
+                    , 118014 ]
 
 printStatusEvery :: Int
 printStatusEvery = 500
@@ -35,10 +126,13 @@ downloadLimitBytes :: Int
 downloadLimitBytes = 50000
 
 concurrentConnectionCount :: Int
-concurrentConnectionCount = 10
+concurrentConnectionCount = 3
 
 responseTimeoutSeconds :: Int
-responseTimeoutSeconds = 60
+responseTimeoutSeconds = 120
+
+threadSleepSeconds :: Int
+threadSleepSeconds = 10
 
 -------------------------------------------------------------------
 -- Helper functions
@@ -72,6 +166,7 @@ threadPoolIO nr mutator = do
     outputChan <- newChan
     forM_ [1..nr] $
         \_ -> forkIO (forever $ do
+            threadDelay $ threadSleepSeconds * 1000000
             i <- readChan inputChan
             o <- mutator i
             writeChan outputChan o)
