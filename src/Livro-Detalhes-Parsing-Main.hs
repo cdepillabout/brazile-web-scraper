02e6ee42be61952022193ba48e396cd0c74918d9
(cdep)illabout <cdep.illabout@gmail.com>
2014-10-07 09:16:14 +0900

    Added template file for actually parsing results.

---
+++ src/Livro-Detalhes-Parsing-Main.hs
@@ -0,0 +1,116 @@
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
+
+import Control.Concurrent (forkIO, threadDelay)
+import Control.Concurrent.Chan (Chan, newChan, readChan, writeChan, writeList2Chan)
+import Control.Exception (catch, IOException)
+import Control.Monad (forever, forM_, when)
+import Data.Functor ((<$>))
+--import Debug.Trace (traceIO)
+import Network.HTTP.Client (BodyReader, brConsume, defaultManagerSettings, HttpException, Manager, managerConnCount, managerResponseTimeout, parseUrl, Request, Response, responseBody, withManager, withResponse)
+import qualified Data.ByteString as ByteStringStrict
+import System.IO (withFile, IOMode(WriteMode))
+import Text.Printf (printf)
+
+
+-------------------------------------------------------------------
+-- Constants
+-------------------------------------------------------------------
+
+allDetailsPath :: String
+allDetailsPath = "all_livrodetalhes_sorted_uniq"
+
+siteUrl :: String
+siteUrl = "http://museudaimigracao.org.br/acervodigital/"
+
+start :: Int
+start = 1
+
+total :: Int
+total = 20
+
+allRequestNumbers :: [Int]
+allRequestNumbers = [start .. total]
+
+printStatusEvery :: Int
+printStatusEvery = 1
+
+concurrentConnectionCount :: Int
+concurrentConnectionCount = 3
+
+responseTimeoutSeconds :: Int
+responseTimeoutSeconds = 120
+
+threadSleepSeconds :: Int
+threadSleepSeconds = 10
+
+-------------------------------------------------------------------
+-- Helper functions
+-------------------------------------------------------------------
+
+outFilePath :: Int -> FilePath
+outFilePath = printf "output/detalhes/%06d.html"
+
+errFilePath :: Int -> FilePath
+errFilePath = printf "output/err/%06d.html"
+
+request :: String -> IO Request
+request url = parseUrl $ siteUrl ++ url
+
+threadPoolIO :: Int -> (a -> IO ()) -> IO (Chan a, Chan ())
+threadPoolIO nr mutator = do
+    inputChan <- newChan
+    outputChan <- newChan
+    forM_ [1..nr] $
+        \_ -> forkIO (forever $ do
+            threadDelay $ threadSleepSeconds * 1000000
+            i <- readChan inputChan
+            o <- mutator i
+            writeChan outputChan o)
+    return (inputChan, outputChan)
+
+-------------------------------------------------------------------
+-- file downloading functions
+-------------------------------------------------------------------
+
+doResponse :: Int -> Response BodyReader -> IO ()
+doResponse reqNum response =
+    catch (doResponse' >> printStatus) errHandler
+  where
+    printStatus :: IO ()
+    printStatus = when (reqNum `mod` printStatusEvery == 0) $ print reqNum
+
+    doResponse' :: IO ()
+    doResponse' = withFile (outFilePath reqNum) WriteMode $ \fileHandle -> do
+              let lazybody = responseBody response
+              bytestrings <- brConsume lazybody
+              forM_ bytestrings $ \bs -> ByteStringStrict.hPut fileHandle bs
+              ByteStringStrict.hPut fileHandle "\n"
+
+    errHandler :: IOException -> IO ()
+    errHandler err = writeFile (errFilePath reqNum) $ show err ++ "\n"
+
+doRequest :: (Manager, Int, String) -> IO ()
+doRequest (manager, reqNum, reqUrl) = do
+    req <- request reqUrl
+    catch (withResponse req manager $ doResponse reqNum)
+          (\err -> writeFile (errFilePath reqNum) $ show (err::HttpException) ++ "\n")
+
+doRequestsThreadPool :: [String] -> Manager -> IO ()
+doRequestsThreadPool allDetailsList manager = do
+    (inputChan, outputChan) <- threadPoolIO concurrentConnectionCount doRequest
+    _ <- writeList2Chan inputChan $ zip3 (repeat manager) allRequestNumbers allDetailsList
+    mapM_ (\_ -> readChan outputChan) allRequestNumbers
+
+-------------------------------------------------------------------
+-- main
+-------------------------------------------------------------------
+
+main :: IO ()
+main = do
+    let managerSettings = defaultManagerSettings
+          { managerConnCount = concurrentConnectionCount
+          , managerResponseTimeout = Just $ responseTimeoutSeconds * 1000000
+          }
+    allDetailsList <- lines <$> readFile allDetailsPath
+    withManager managerSettings $ doRequestsThreadPool allDetailsList
+
