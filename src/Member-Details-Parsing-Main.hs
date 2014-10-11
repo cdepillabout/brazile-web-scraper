b8383c6e3d5a0d0037dffd133fac332f87be078a
(cdep)illabout <cdep.illabout@gmail.com>
2014-10-11 13:20:02 +0900

    Finished the member details parser.

--- src/Member-Details-Parsing-Main.hs
+++ src/Member-Details-Parsing-Main.hs
@@ -11,7 +11,7 @@ import qualified Data.Text.IO as TextIO
 import System.IO (hPutStrLn, stderr)
 import qualified Text.HTML.DOM as TextHTMLDOM
 import Text.Printf (printf)
-import Text.XML.Cursor (($//), (&/), (&//), (>=>), attributeIs, child, content, Cursor, element, fromDocument)
+import Text.XML.Cursor (($//), (&|), (&/), (&//), (>=>), attributeIs, child, content, Cursor, element, fromDocument)
 
 
 -------------------------------------------------------------------
@@ -22,8 +22,7 @@ start :: Int
 start = 1
 
 total :: Int
---total = 20
-total = 1
+total = 20
 
 allRequestNumbers :: [Int]
 allRequestNumbers = [start .. total]
@@ -59,33 +58,45 @@ chunksOf num = go
                  (a,b) | null a    -> []
                        | otherwise -> a : go b
 
+textShow :: Show a => a -> Text.Text
+textShow = Text.pack . show
+
 -------------------------------------------------------------------
 -- file downloading functions
 -------------------------------------------------------------------
 
 doParse :: Int -> IO ()
-doParse reqNum = doParse'
+doParse reqNum = do
+    document <- TextHTMLDOM.readFile $ fromString $ inFilePath reqNum
+    let cursor = fromDocument document
+    let allContent = cursor $// findNodes &| child &| content
+    let familyData = concat $ concatMap (\x -> if null x then [[" "]] else x) allContent
+    process familyData
   where
-    doParse' :: IO ()
-    doParse' = do
-      document <- TextHTMLDOM.readFile $ fromString $ inFilePath reqNum
-      let cursor = fromDocument document
-      let familyData = cursor $// findNodes &/ content
+    process :: [Text.Text] -> IO()
+    process familyData = do
+      checkNull familyData
+      --print $ length familyData
+      --print familyData
+      let chunkedFamilyData = chunksOf 13 familyData
+      forM_ chunkedFamilyData processChunk
+
+    checkNull :: [Text.Text] -> IO ()
+    checkNull familyData =
       when (null familyData) $ do
         let familyDataString = show familyData
             errorMessage = "error: length for familyData from " ++ show reqNum ++ " is 0: "
         hPutStrLn stderr $ errorMessage ++ familyDataString
-      --print $ length familyData
-      print familyData
-      let chunkedFamilyData = chunksOf 13 familyData
-      forM_ chunkedFamilyData $ \chunk ->
-        if (length chunk /= 13)
-          then
-            let chunkString = show chunk
-                errorMessage = "error: length of chunk from " ++ show reqNum ++ " is not 13: "
-            in hPutStrLn stderr $ errorMessage ++ chunkString
-          else
-            TextIO.putStrLn $ Text.intercalate " ; " $ map Text.strip chunk
+
+    processChunk :: [Text.Text] -> IO ()
+    processChunk chunk =
+      if length chunk /= 13
+        then
+          let chunkString = show chunk
+              errorMessage = "error: length of chunk from " ++ show reqNum ++ " is not 13: "
+          in hPutStrLn stderr $ errorMessage ++ chunkString
+        else
+          TextIO.putStrLn $ Text.intercalate " ; " $ map Text.strip $ textShow reqNum : chunk
 
     findNodes :: Cursor -> [Cursor]
     --findNodes = element "tr" >=> child &/ element "td"
@@ -94,7 +105,8 @@ doParse reqNum = doParse'
             element "tr" &/
                 element "td" >=> attributeIs "class" "texto" >=> attributeIs "align" "left"
 
--- concat $ concatMap (\x -> if x == [] then [[" "]] else x) $ map (child &| content) $ cursor $// element "table" >=> attributeIs "width" "908" &// element "tr" &/ element "td" >=> attributeIs "class" "texto" >=> attributeIs "align" "left"
+
+
 
 doParseWrapper :: Int -> IO ()
 doParseWrapper reqNum =
